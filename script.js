e4850aea253c98b2bae0321ce10c1c811d41b82f
mmyleung <minamleung@gmail.com>
2023-05-25 11:21:09 +0100

    ajax call to openweathermap api and receive data according to city name input

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-
-
 //add event listener to form for submit
 $("#search-form").on("submit", function(event) {
     event.preventDefault();
     //store input value into variable
-    var city = $("#search-input").val();
-    console.log(city);
+    var cityName = $("#search-input").val().trim();
+    //apiKey
+    var APIkey = "0fdfab0bcf3590a8e7a2c9aecb8d3388"
+    //use geocoding API to return weather by city name
+    //query url for calling openweathermap API using city name and api key
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
+
+    console.log(cityName);
+    //calling ajax to get data from openweathermap API
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(data) {
+        console.log(data);
+    })
 })
+
+
