73928d01725e0d5738cf2fe54e479a763d1939e8
mmyleung <minamleung@gmail.com>
2023-05-25 11:40:53 +0100

    request for data in metric system and store temperature in variable

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $("#search-form").on("submit", function(event) {
     var APIkey = "0fdfab0bcf3590a8e7a2c9aecb8d3388"
     //use geocoding API to return weather by city name
     //query url for calling openweathermap API using city name and api key
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey + "&units=metric";
 
     console.log(cityName);
     //calling ajax to get data from openweathermap API
@@ -21,6 +21,8 @@ $("#search-form").on("submit", function(event) {
         var icon = data.weather[0].icon;
         var iconContainer = $("<img>").attr("src", `https://openweathermap.org/img/wn/${icon}.png`);
         $("#today").append(iconContainer);
+        var temperature = data.main.temp;
+        console.log(temperature);
     })
 })
 
